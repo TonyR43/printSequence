a3db7aea5624452bbb5032148e888af5e30ce2c8
Tony <tony.ratley430@gmail.com>
2019-02-07 21:54:35 -0600

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main
+    {
+        
+        public static void main(String[] args)
+            {
+                int num = 100;
+                while (num >= -100)
+                {
+                    System.out.println(num);
+                    num = num - 8;
+                }
+            }
+    }
